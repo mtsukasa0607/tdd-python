5366f8cd9764bf6a14650c37167ed5bc9f9421dd
mtsukasa0607 <m.tsukasa0607@gmail.com>
2021-04-29 22:53:40 +0900

    Clean xUnit20

--- xUnit20/xunit.py
+++ xUnit20/xunit.py
@@ -1,54 +1,71 @@
 import unittest
 
 class TestCase:
-    # pass
     def __init__(self, name):
-        # self.wasRun = None
         self.name = name
+        # self.wasRun = None
     def setUp(self):
+        pass
+    def tearDown(self):
         pass
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
+        # self.log = self.log + "tearDown"
 
 class WasRun(TestCase):
-    # pass
+    def setUp(self):
+        self.log = "setUp "
+        # self.wasRun = None
+        # self.wasSetUp = 1
+    def testMethod(self):
+        self.log = self.log + "testMethod "
+        # pass
+        # self.wasRun = 1
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+        # pass
+        # self.wasRun = 1
     # def __init__(self, name):
         # pass
         # self.wasRun = None
         # self.name = name
         # super().__init__(name)
-    def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
     # def run(self):
         # self.testMethod()
         # method = getattr(self, self.name)
         # method()
-    def testMethod(self):
-        # pass
-        self.wasRun = 1
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-    def testRunning(self):
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
+        # self.test.run()
+        # assert("setUp testMethod" == self.test.log)
+        # assert("setUp testMethod" == test.log)
+    # def setUp(self):
+        # self.test = WasRun("testMethod")
+    # def testRunning(self):
         # test = WasRun("testMethod")
         # assert(not test.wasRun)
         # test.run()
         # assert(test.wasRun)
-        self.test.run()
-        assert(self.test.wasRun)
-    def testSetUp(self):
+        # self.test.run()
+        # assert(self.test.wasRun)
+    # def testSetUp(self):
         # test = WasRun("testMethod")
         # test.run()
         # assert(test.wasSetUp)
-        self.test.run()
-        assert(self.test.wasSetUp)
+        # self.test.run()
+        # assert("setUp" == self.test.log)
+        # assert("setUp testMethod" == self.test.log)
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
+# TestCaseTest("testRunning").run()
+# TestCaseTest("testSetUp").run()
 
 # test = WasRun("testMethod")
 # print(test.wasRun)
