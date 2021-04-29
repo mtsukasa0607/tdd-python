4c8af5c65e0003d9864d12b59ce314d772a13b39
mtsukasa0607 <m.tsukasa0607@gmail.com>
2021-04-29 16:37:14 +0900

    Add xUnit19

---
+++ xUnit19/xunit.py
@@ -0,0 +1,39 @@
+import unittest
+
+class TestCase:
+    # pass
+    def __init__(self, name):
+        # self.wasRun = None
+        self.name = name
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+class WasRun(TestCase):
+    # pass
+    def __init__(self, name):
+        # pass
+        self.wasRun = None
+        # self.name = name
+        super().__init__(name)
+    # def run(self):
+        # self.testMethod()
+        # method = getattr(self, self.name)
+        # method()
+    def testMethod(self):
+        # pass
+        self.wasRun = 1
+
+class TestCaseTest (TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+
+TestCaseTest("testRunning").run()
+
+# test = WasRun("testMethod")
+# print(test.wasRun)
+# test.run()
+# print(test.wasRun)
