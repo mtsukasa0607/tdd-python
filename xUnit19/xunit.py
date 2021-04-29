e311c25d363c22e1cdb99393494221a85b75963d
mtsukasa0607 <m.tsukasa0607@gmail.com>
2021-04-29 19:48:35 +0900

    Clean xUnit19

--- xUnit19/xunit.py
+++ xUnit19/xunit.py
@@ -5,17 +5,23 @@ class TestCase:
     def __init__(self, name):
         # self.wasRun = None
         self.name = name
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 class WasRun(TestCase):
     # pass
-    def __init__(self, name):
+    # def __init__(self, name):
         # pass
-        self.wasRun = None
+        # self.wasRun = None
         # self.name = name
-        super().__init__(name)
+        # super().__init__(name)
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
     # def run(self):
         # self.testMethod()
         # method = getattr(self, self.name)
@@ -24,14 +30,25 @@ class WasRun(TestCase):
         # pass
         self.wasRun = 1
 
-class TestCaseTest (TestCase):
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        # test = WasRun("testMethod")
+        # assert(not test.wasRun)
+        # test.run()
+        # assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+    def testSetUp(self):
+        # test = WasRun("testMethod")
+        # test.run()
+        # assert(test.wasSetUp)
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
 
 # test = WasRun("testMethod")
 # print(test.wasRun)
